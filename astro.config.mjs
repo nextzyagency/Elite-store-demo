9b4b1c0d4a8fc70070ade510fc19e1c4cc50de4f
gabriel <nextzyagency@gmail.com>
2026-04-11 21:40:21 -0500

    Fix: Cloudflare server adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,12 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
-import node from '@astrojs/node';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
+  adapter: cloudflare(),
   vite: {
     plugins: [tailwindcss()]
   },
